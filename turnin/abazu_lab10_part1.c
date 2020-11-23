b290f1e6adb1be70c27330059185b18f380d6e23
Okubyona <andrewbazua@gmail.com>
2020-11-22 19:25:56 -0800

    refactor: update part 1 with demo link

--- turnin/abazu_lab10_part1.c
+++ turnin/abazu_lab10_part1.c
@@ -1,4 +1,21 @@
-
+/*	Author: abazu001
+ *  Partner(s) Name:
+ *	Lab Section:
+ *	Assignment: Lab #10  Exercise #1
+ *	Exercise Description: [ Connect LEDs to PB0, PB1, PB2, and PB3.
+        In one state machine (ThreeLEDsSM), output to a shared variable
+            (threeLEDs) the following behavior: set only bit 0 to 1, then only
+            bit 1, then only bit 2 in sequence for 1 second each.
+        In a second state machine (BlinkingLEDSM), output to a shared variable
+            (blinkingLED) the following behavior: set bit 3 to 1 for 1 second,
+            then 0 for 1 second.
+        In a third state machine (CombineLEDsSM), combine both shared variables
+            and output to the PORTB. ]
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ *  Demo Link:
+ *  https://drive.google.com/file/d/1B9RFMQsN-EDprh8B-S6Usr4YiOrnPPoB/view?usp=sharing
+ */
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #ifdef _SIMULATE_
